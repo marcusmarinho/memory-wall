f39afb65aee0abcd173ea98f3266c0836a311fec
marcus marinho <marcuspratamarinho@gmail.com>
2025-01-22 18:07:18 -0300

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/{memory-wall}',
+  base: '/memory-wall',
   plugins: [react()],
 });
