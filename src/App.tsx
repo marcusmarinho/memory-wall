016738a97c0886d28a8d764830fe371e59300466
marcus marinho <marcuspratamarinho@gmail.com>
2025-01-24 21:33:53 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import { ProjectRoutes } from './routes';
 function App() {
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.location.replace(`${window.location.pathname}#/`);
+    }
+  }, []);
   return <ProjectRoutes></ProjectRoutes>;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './core.css';
 import App from './App.tsx';
 
-if (!window.location.hash) {
-  window.location.replace(`${window.location.pathname}#/`);
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
