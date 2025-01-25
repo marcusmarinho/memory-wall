212a2534f96f5e3facda2517fb095fa8e722a77d
marcus marinho <marcuspratamarinho@gmail.com>
2025-01-24 21:31:32 -0300

    add hash

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './core.css';
 import App from './App.tsx';
 
+if (!window.location.hash) {
+  window.location.replace(`${window.location.pathname}#/`);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage, LoginPage } from '../pages';
 
 export const ProjectRoutes = () => {
   return (
-    <BrowserRouter basename='/memory-wall'>
+    <Router basename='/memory-wall'>
       <Routes>
         <Route path='/' element={<LoginPage></LoginPage>}></Route>
         <Route path='/home' element={<HomePage></HomePage>}></Route>
         <Route path='*' element={<HomePage></HomePage>}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
